60cdb8edbb9fe13ac35c4f806bb73488cd4991be
Aurelien Requiem <aurelien@requiem.fr>
2022-11-21 14:44:16 +1100

    Check that any security group doesn't allow inbound HTTP traffic

--- policies/aws/ec2/securityGroup.ts
+++ policies/aws/ec2/securityGroup.ts
@@ -8,9 +8,25 @@ export const awsEc2SecurityGroupMissingDescription: ResourceValidationPolicy = {
     name: "add-desciption-to-security-group",
     description: "Checks that all security groups have a description.",
     enforcementLevel: "advisory",
-    validateResource: validateResourceOfType( aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
+    validateResource: validateResourceOfType(aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
         if (!sg.description) {
             reportViolation("Security group must have a description.");
         }
+    }),
+};
+
+export const awsEc2SecurityGroupNoInboundHttpTraffic: ResourceValidationPolicy = {
+    name: "disallow-inbound-http-traffic",
+    description: "Check that any security group doesn't allow inbound HTTP traffic.",
+    enforcementLevel: "advisory",
+    validateResource: validateResourceOfType(aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
+        sg.ingress?.forEach((ingress) => {
+            if (ingress.protocol.toLowerCase() == "tcp" && (ingress.fromPort == 80 || ingress.toPort == 80)) {
+                reportViolation("A security group ingress rule allows inbound HTTP traffic.");
+            }
+            if (ingress.protocol.toLowerCase() == "tcp" && (ingress.fromPort < 80 && ingress.toPort > 80)) {
+                reportViolation("A security group ingress rule allows inbound HTTP traffic.");
+            }
+        });
     }),
 };

c12dc728ccfe65fbf76d6000b5b7f02f511b6618
Aurelien Requiem <aurelien@requiem.fr>
2022-11-21 17:43:08 +1100

    add policies for ebs volumes

---
+++ policies/aws/ebs/volume.ts
@@ -0,0 +1,26 @@
+import {
+    ResourceValidationPolicy,
+    validateResourceOfType,
+} from "@pulumi/policy";
+import * as aws from "@pulumi/aws";
+
+export const awsEbsVolumeNoUnencryptedVolume: ResourceValidationPolicy = {
+    name: "disallow-unencrypted-volume",
+    description: "Checks that no EBS is unencrypted.",
+    enforcementLevel: "advisory",
+    validateResource: validateResourceOfType(aws.ebs.Volume, (v, args, reportViolation) => {
+        if (!v.encrypted) {
+            reportViolation("An EBS volume is not encrypted.");
+        }
+    }),
+};
+
+export const awsEbsVolumeWithCustomerManagedKey: ResourceValidationPolicy = {
+    name: "disallow-volume-without-customer-managed-key",
+    description: "Check that encrypted EBS volume uses a customer-manager KMS key.",
+    validateResource: validateResourceOfType(aws.ebs.Volume, (v, args, reportViolation) => {
+        if (v.kmsKeyId !== undefined && v.kmsKeyId !== "") {
+            reportViolation("An EBS volume should be encrypted with a customer-managed KMS key.");
+        }
+    }),
+};

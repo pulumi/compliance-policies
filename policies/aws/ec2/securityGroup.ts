81562aa720bff4dba8c76ad6bd18a018e84288b7
Aurelien Requiem <aurelien@requiem.fr>
2022-11-21 13:39:46 +1100

    retab

--- policies/aws/ec2/securityGroup.ts
+++ policies/aws/ec2/securityGroup.ts
@@ -1,16 +1,16 @@
 import {
-  ResourceValidationPolicy,
-  validateResourceOfType,
+    ResourceValidationPolicy,
+    validateResourceOfType,
 } from "@pulumi/policy";
 import * as aws from "@pulumi/aws";
 
 export const awsEc2SecurityGroupMissingDescription: ResourceValidationPolicy = {
-  name: "add-desciption-to-security-group",
-  description: "Checks that all security groups have a description.",
-  enforcementLevel: "advisory",
-  validateResource: validateResourceOfType( aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
-    if (!sg.description) {
-      reportViolation("Security group must have a description.");
-    }
-  }),
+    name: "add-desciption-to-security-group",
+    description: "Checks that all security groups have a description.",
+    enforcementLevel: "advisory",
+    validateResource: validateResourceOfType( aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
+        if (!sg.description) {
+            reportViolation("Security group must have a description.");
+        }
+    }),
 };

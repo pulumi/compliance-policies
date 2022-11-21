e595fc35698358b5d1f4c6f874be8e356e6d7936
Aurelien Requiem <aurelien@requiem.fr>
2022-11-21 13:22:54 +1100

    rename rules reformat code

--- policies/aws/ec2/securityGroup.ts
+++ policies/aws/ec2/securityGroup.ts
@@ -4,22 +4,13 @@ import {
 } from "@pulumi/policy";
 import * as aws from "@pulumi/aws";
 
-const description: ResourceValidationPolicy = {
+export const awsEc2SecurityGroupMissingDescription: ResourceValidationPolicy = {
   name: "add-desciption-to-security-group",
   description: "Checks that all security groups have a description.",
   enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.SecurityGroup,
-    (sg, args, reportViolation) => {
-      if (!sg.description) {
-        reportViolation("Security group must have a description.");
-      }
+  validateResource: validateResourceOfType( aws.ec2.SecurityGroup, (sg, args, reportViolation) => {
+    if (!sg.description) {
+      reportViolation("Security group must have a description.");
     }
-  ),
+  }),
 };
-
-
-
-export default [
-    description,
-];
--- policies/aws/ec2/volumes.ts
+++ policies/aws/ec2/volumes.ts
@@ -4,98 +4,63 @@ import {
 } from "@pulumi/policy";
 import * as aws from "@pulumi/aws";
 
-const launchConfigRootBlockDevice: ResourceValidationPolicy = {
+export const awsEc2LaunchConfigurationNoUnencryptedRootBlockDevice: ResourceValidationPolicy = {
   name: "disallow-unencrypted-root-volume-on-launch-config",
-  description:
-    "Checks that any launch configuration do not have unencrypted root volumes.",
+  description: "Checks that any launch configuration do not have unencrypted root volumes.",
   enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.LaunchConfiguration,
-    (lc, args, reportViolation) => {
-      if (!lc.rootBlockDevice?.encrypted) {
-        reportViolation(
-          "The root block device for this launch configuration is not encrypted."
-        );
-      }
+  validateResource: validateResourceOfType(aws.ec2.LaunchConfiguration, (lc, args, reportViolation) => {
+    if (!lc.rootBlockDevice?.encrypted) {
+      reportViolation("The root block device for this launch configuration is not encrypted.");
     }
-  ),
+  }),
 };
 
-const launchConfigBlockDevices: ResourceValidationPolicy = {
+export const awsEc2LaunchConfigurationNoUnencryptedBlockDevice: ResourceValidationPolicy = {
   name: "disallow-unencrypted-volumes-on-launch-config",
-  description:
-    "Checks that any launch configuration do not have unencrypted volumes.",
+  description: "Checks that any launch configuration do not have unencrypted volumes.",
   enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.LaunchConfiguration,
-    (lc, args, reportViolation) => {
-      lc.ebsBlockDevices?.forEach((device) => {
-        if (!device.encrypted) {
-          reportViolation(
-            "A block device for this launch configuration is not encrypted."
-          );
-        }
-      });
-    }
-  ),
-};
-
-const instanceRootBlockDevice: ResourceValidationPolicy = {
-  name: "disallow-unencrypted-root-volume-on-instance",
-  description:
-    "Checks that any EC2 instance does not have unencrypted root volumes.",
-  enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.Instance,
-    (i, args, reportViolation) => {
-      if (!i.rootBlockDevice?.encrypted) {
-        reportViolation(
-          "The root block device for this instance is not encrypted."
-        );
+  validateResource: validateResourceOfType(aws.ec2.LaunchConfiguration, (lc, args, reportViolation) => {
+    lc.ebsBlockDevices?.forEach((device) => {
+      if (!device.encrypted) {
+        reportViolation("A block device for this launch configuration is not encrypted.");
       }
-    }
-  ),
+    });
+  }),
 };
 
-const instanceBlockDevices: ResourceValidationPolicy = {
+export const awsEc2InstanceNoUnencryptedRootBlockDevice: ResourceValidationPolicy = {
+  name: "disallow-unencrypted-root-volume-on-instance",
+  description: "Checks that any EC2 instance does not have unencrypted root volumes.",
+  enforcementLevel: "advisory",
+  validateResource: validateResourceOfType(aws.ec2.Instance, (i, args, reportViolation) => {
+    if (!i.rootBlockDevice?.encrypted) {
+      reportViolation("The root block device for this instance is not encrypted.");
+    }
+  }),
+};
+
+export const awsEc2InstanceNoUnencryptedBlockDevice: ResourceValidationPolicy = {
   name: "disallow-unencrypted-volumes-on-launch-config",
   description: "Checks that any EC2 instances do not have unencrypted volumes.",
   enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.Instance,
-    (i, args, reportViolation) => {
-      i.ebsBlockDevices?.forEach((device) => {
-        if (!device.encrypted) {
-          reportViolation("A block device for this instance is not encrypted.");
-        }
-      });
-    }
-  ),
+  validateResource: validateResourceOfType(aws.ec2.Instance, (i, args, reportViolation) => {
+    i.ebsBlockDevices?.forEach((device) => {
+      if (!device.encrypted) {
+        reportViolation("A block device for this instance is not encrypted.");
+      }
+    });
+  }),
 };
 
-const launchTemplateBlockDevices: ResourceValidationPolicy = {
+export const awsEc2LaunchTemplateNoUnencryptedBlockDevice: ResourceValidationPolicy = {
   name: "disallow-unencrypted-volumes-on-launch-template",
-  description:
-    "Checks that any launch templates do not have unencrypted root volumes.",
+  description: "Checks that any launch templates do not have unencrypted root volumes.",
   enforcementLevel: "advisory",
-  validateResource: validateResourceOfType(
-    aws.ec2.LaunchTemplate,
-    (lt, args, reportViolation) => {
-      lt.blockDeviceMappings?.forEach((device) => {
-        if (!device.ebs?.encrypted) {
-          reportViolation(
-            "A block device for this launch template is not encrypted."
-          );
-        }
-      });
-    }
-  ),
+  validateResource: validateResourceOfType(aws.ec2.LaunchTemplate, (lt, args, reportViolation) => {
+    lt.blockDeviceMappings?.forEach((device) => {
+      if (!device.ebs?.encrypted) {
+        reportViolation("A block device for this launch template is not encrypted.");
+      }
+    });
+  }),
 };
-
-export default [
-  launchConfigRootBlockDevice,
-  launchConfigBlockDevices,
-  instanceRootBlockDevice,
-  instanceBlockDevices,
-  launchTemplateBlockDevices,
-];

c03c4f030cf4a88cd133f3f1256bd99ac8c11ab2
Aurelien Requiem <aurelien@requiem.fr>
2022-11-26 16:13:23 +1100

    add package version

--- policies/version.ts
+++ policies/version.ts
@@ -1,4 +1,4 @@
-// Copyright 2016-2022, Pulumi Corporation.
+// Copyright 2016-2019, Pulumi Corporation.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,5 +12,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * as aws from "./aws";
-export * from "./version";
+// The version that the @pulumi/pulumi-policies package was built as.
+export const version = "${VERSION}";

261c1c9ec329570d77018ae2ffb2ed9edb7d6a9f
Carl Holzboog <caboog@gmail.com>
2022-12-07 17:06:38 -0800

    Making the TS imoort statements for aws-native ugly AKA awsnative

--- policies/aws-native/secretsmanager/secret.ts
+++ policies/aws-native/secretsmanager/secret.ts
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as awsNative from "@pulumi/aws-native";
+import * as awsnative from "@pulumi/aws-native";
 import {
     ResourceValidationPolicy,
     validateResourceOfType,

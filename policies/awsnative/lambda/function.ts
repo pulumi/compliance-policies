d252dbdf2bc7e1bf1900b04a6a60bd7ca9c1ef9d
Aurelien Requiem <aurelien@requiem.fr>
2022-12-24 13:58:33 +1100

    📦 awsnative.lambda.Function
    ✨ add new policy
    🐛 use correct property types

--- policies/awsnative/lambda/function.ts
+++ policies/awsnative/lambda/function.ts
@@ -20,6 +20,29 @@ import {
 import { policyRegistrations } from "../../utils";
 
 /**
+ * Checks that all Lambda Functions have a description.
+ *
+ * @severity **Low**
+ * @link https://docs.aws.amazon.com/lambda/latest/dg/getting-started.html
+ */
+export const missingDescription: ResourceValidationPolicy = policyRegistrations.registerPolicy({
+    resourceValidationPolicy: {
+        name: "awsnative-lambda-function-missing-description",
+        description: "Checks that all Lambda Functions have a description.",
+        enforcementLevel: "advisory",
+        validateResource: validateResourceOfType(awsnative.lambda.Function, (f, args, reportViolation) => {
+            if (!f.description) {
+                reportViolation("Lambda functions should have a description.");
+            }
+        }),
+    },
+    vendors: ["aws"],
+    services: ["lambda"],
+    severity: "low",
+    topics: ["documentation"],
+});
+
+/**
  * Checks that Lambda functions have tracing enabled.
  *
  * @severity **Low**
@@ -54,7 +77,7 @@ export const configureTracingConfig: ResourceValidationPolicy = policyRegistrati
         description: "Checks that Lambda functions have tracing configured.",
         enforcementLevel: "advisory",
         validateResource: validateResourceOfType(awsnative.lambda.Function, (f, args, reportViolation) => {
-            if (f.tracingConfig && f.tracingConfig.mode !== "Active") {
+            if (f.tracingConfig && f.tracingConfig.mode !== awsnative.lambda.FunctionTracingConfigMode.Active) {
                 reportViolation("Lambda functions should have tracing configured.");
             }
         }),

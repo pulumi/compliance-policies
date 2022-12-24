b5497f4564d9550d3e736ed1a6efa653528a993f
Aurelien Requiem <aurelien@requiem.fr>
2022-12-24 14:11:25 +1100

    ✨ add unit tests for awsnative.eks.Cluster

---
+++ policies/tests/awsnative/eks/cluster.spec.ts
@@ -0,0 +1,128 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import "mocha";
+import { assertHasResourceViolation, assertNoResourceViolations, assertResourcePolicyIsRegistered, assertResourcePolicyRegistrationDetails, createResourceValidationArgs, assertResourcePolicyName, assertResourcePolicyEnforcementLevel, assertResourcePolicyDescription } from "../../utils";
+import * as awsnative from "@pulumi/aws-native";
+
+import * as policies from "../../../index";
+import { ResourceValidationArgs } from "@pulumi/policy";
+import { ec2, iam, kms } from "../enums";
+
+function getResourceValidationArgs(): ResourceValidationArgs {
+    return createResourceValidationArgs(awsnative.eks.Cluster, {
+        roleArn: iam.roleArn,
+        resourcesVpcConfig: {
+            subnetIds: [
+                ec2.subnetId1,
+                ec2.subnetId2,
+            ],
+            endpointPublicAccess: false,
+            endpointPrivateAccess: true,
+        },
+        encryptionConfig: [{
+            provider: {
+                keyArn: kms.keyArn,
+            },
+            resources: ["secrets"]
+        }]
+    });
+}
+
+describe("awsnative.eks.Cluster.enableClusterEncryptionConfig", () => {
+    const policy = policies.awsnative.eks.Cluster.enableClusterEncryptionConfig;
+
+    it("name", async () => {
+        assertResourcePolicyName(policy, "awsnative-eks-cluster-enable-cluster-encryption-config");
+    });
+
+    it("registration", async () => {
+        assertResourcePolicyIsRegistered(policy);
+    });
+
+    it("metadata", async () => {
+        assertResourcePolicyRegistrationDetails(policy, {
+            vendors: ["aws"],
+            services: ["eks"],
+            severity: "high",
+            topics: ["encryption", "kubernetes"],
+        });
+    });
+
+    it("enforcementLevel", async () => {
+        assertResourcePolicyEnforcementLevel(policy);
+    });
+
+    it("description", async () => {
+        assertResourcePolicyDescription(policy);
+    });
+
+    it("#1", async () => {
+        const args = getResourceValidationArgs();
+        await assertNoResourceViolations(policy, args);
+    });
+
+    it("#2", async () => {
+        const args = getResourceValidationArgs();
+        args.props.encryptionConfig = undefined;
+        await assertHasResourceViolation(policy, args, { message: "EKS Cluster Encryption Configuration should be enabled." });
+    });
+});
+
+describe("awsnative.eks.Cluster.disallowAPIEndpointPublicAccess", () => {
+    const policy = policies.awsnative.eks.Cluster.disallowAPIEndpointPublicAccess;
+
+    it("name", async () => {
+        assertResourcePolicyName(policy, "awsnative-eks-cluster-disallow-api-endpoint-public-access");
+    });
+
+    it("registration", async () => {
+        assertResourcePolicyIsRegistered(policy);
+    });
+
+    it("metadata", async () => {
+        assertResourcePolicyRegistrationDetails(policy, {
+            vendors: ["aws"],
+            services: ["eks"],
+            severity: "critical",
+            topics: ["network"],
+        });
+    });
+
+    it("enforcementLevel", async () => {
+        assertResourcePolicyEnforcementLevel(policy);
+    });
+
+    it("description", async () => {
+        assertResourcePolicyDescription(policy);
+    });
+
+    it("#1", async () => {
+        const args = getResourceValidationArgs();
+        await assertNoResourceViolations(policy, args);
+    });
+
+    it("#2", async () => {
+        const args = getResourceValidationArgs();
+        args.props.resourcesVpcConfig.endpointPublicAccess = undefined;
+        await assertHasResourceViolation(policy, args, { message: "EKS Cluster Encryption API endpoint should not be publicly accessible." });
+    });
+
+    it("#3", async () => {
+        const args = getResourceValidationArgs();
+        args.props.resourcesVpcConfig.endpointPublicAccess = undefined;
+        args.props.resourcesVpcConfig.publicAccessCidrs = [ "0.0.0.0/0" ];
+        await assertHasResourceViolation(policy, args, { message: "EKS Cluster Encryption API endpoint should not be publicly accessible." });
+    });
+});

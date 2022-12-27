14c6f50e95a8af750495056deba3e01c39d19d4e
Aurelien Requiem <aurelien@requiem.fr>
2022-12-28 10:43:58 +1100

    ✨ add 1 policy for awsnative.apigateway.DomainName

--- policies/awsnative/apigateway/index.ts
+++ policies/awsnative/apigateway/index.ts
@@ -12,13 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * as apigateway from "./apigateway";
-export * as cloudfront from "./cloudfront";
-export * as ec2 from "./ec2";
-export * as ecr from "./ecr";
-export * as efs from "./efs";
-export * as eks from "./eks";
-export * as kms from "./kms";
-export * as lambda from "./lambda";
-export * as rds from "./rds";
-export * as s3 from "./s3";
+export * as DomainName from "./domainName";

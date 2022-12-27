ced087451c54c17ac56b9ba151622e442ca1d89e
Aurelien Requiem <aurelien@requiem.fr>
2022-12-27 22:15:03 +1100

    ✨ add 2 new policies for AWS ALB LoadBalancer

---
+++ policies/aws/alb/loadBalancer.ts
@@ -0,0 +1,66 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import * as aws from "@pulumi/aws";
+import {
+    ResourceValidationPolicy,
+    validateResourceOfType,
+} from "@pulumi/policy";
+import { policiesManagement } from "../../utils";
+
+/**
+ * Checks that ALB loadbalancer have access logging enabled.
+ *
+ * @severity **medium**
+ * @link https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html
+ */
+export const enableAccessLogging: ResourceValidationPolicy = policiesManagement.registerPolicy({
+    resourceValidationPolicy: {
+        name: "aws-alb-loadbalancer-enable-access-logging",
+        description: "Checks that ALB loadbalancers have access logging enabled.",
+        enforcementLevel: "advisory",
+        validateResource: validateResourceOfType(aws.alb.LoadBalancer, (loadBalancer, args, reportViolation) => {
+            if (!loadBalancer.accessLogs) {
+                reportViolation("ALB LoadBalancers should have access logging enabled.");
+            }
+        }),
+    },
+    vendors: ["aws"],
+    services: ["alb"],
+    severity: "medium",
+    topics: ["network", "logging"],
+});
+
+/**
+ * Checks that ALB loadbalancers have access logging configured and enabled.
+ *
+ * @severity **medium**
+ * @link https://docs.aws.amazon.com/elasticloadbalancing/latest/application/load-balancer-access-logs.html
+ */
+export const configureAccessLogging: ResourceValidationPolicy = policiesManagement.registerPolicy({
+    resourceValidationPolicy: {
+        name: "aws-alb-loadbalancer-configure-access-logging",
+        description: "Checks that ALB loadbalancers have access logging configured and enabled.",
+        enforcementLevel: "advisory",
+        validateResource: validateResourceOfType(aws.alb.LoadBalancer, (loadBalancer, args, reportViolation) => {
+            if (loadBalancer.accessLogs && (!loadBalancer.accessLogs.enabled || !loadBalancer.accessLogs.bucket)) {
+                reportViolation("ALB LoadBalancers should have access logging configured and enabled.");
+            }
+        }),
+    },
+    vendors: ["aws"],
+    services: ["alb"],
+    severity: "medium",
+    topics: ["network", "logging"],
+});
---
+++ policies/tests/aws/alb/loadBalancer.spec.ts
@@ -0,0 +1,118 @@
+// Copyright 2016-2022, Pulumi Corporation.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import "mocha";
+import { assertHasResourceViolation, assertNoResourceViolations, assertResourcePolicyIsRegistered, assertResourcePolicyRegistrationDetails, createResourceValidationArgs, assertResourcePolicyName, assertResourcePolicyEnforcementLevel, assertResourcePolicyDescription } from "../../utils";
+import * as aws from "@pulumi/aws";
+
+import * as policies from "../../../index";
+import { ResourceValidationArgs } from "@pulumi/policy";
+import { iam, root, s3 } from "../enums";
+
+function getResourceValidationArgs(): ResourceValidationArgs {
+    return createResourceValidationArgs(aws.alb.LoadBalancer, {
+        accessLogs: {
+            bucket: s3.bucketId,
+            prefix: "bucket-prefix",
+            enabled: true,
+        },
+        internal: false,
+    });
+}
+
+describe("aws.alb.LoadBalancer.enableAccessLogging", () => {
+    const policy = policies.aws.alb.LoadBalancer.enableAccessLogging;
+
+    it("name", async () => {
+        assertResourcePolicyName(policy, "aws-alb-loadbalancer-enable-access-logging");
+    });
+
+    it("registration", async () => {
+        assertResourcePolicyIsRegistered(policy);
+    });
+
+    it("metadata", async () => {
+        assertResourcePolicyRegistrationDetails(policy, {
+            vendors: ["aws"],
+            services: ["alb"],
+            severity: "medium",
+            topics: ["network", "logging"],
+        });
+    });
+
+    it("enforcementLevel", async () => {
+        assertResourcePolicyEnforcementLevel(policy);
+    });
+
+    it("description", async () => {
+        assertResourcePolicyDescription(policy);
+    });
+
+    it("#1", async () => {
+        const args = getResourceValidationArgs();
+        await assertNoResourceViolations(policy, args);
+    });
+
+    it("#2", async () => {
+        const args = getResourceValidationArgs();
+        args.props.accessLogs = undefined;
+        await assertHasResourceViolation(policy, args, { message: "ALB LoadBalancers should have access logging enabled." });
+    });
+});
+
+describe("aws.alb.LoadBalancer.configureAccessLogging", () => {
+    const policy = policies.aws.alb.LoadBalancer.configureAccessLogging;
+
+    it("name", async () => {
+        assertResourcePolicyName(policy, "aws-alb-loadbalancer-configure-access-logging");
+    });
+
+    it("registration", async () => {
+        assertResourcePolicyIsRegistered(policy);
+    });
+
+    it("metadata", async () => {
+        assertResourcePolicyRegistrationDetails(policy, {
+            vendors: ["aws"],
+            services: ["alb"],
+            severity: "medium",
+            topics: ["network", "logging"],
+        });
+    });
+
+    it("enforcementLevel", async () => {
+        assertResourcePolicyEnforcementLevel(policy);
+    });
+
+    it("description", async () => {
+        assertResourcePolicyDescription(policy);
+    });
+
+    it("#1", async () => {
+        const args = getResourceValidationArgs();
+        await assertNoResourceViolations(policy, args);
+    });
+
+    it("#2", async () => {
+        const args = getResourceValidationArgs();
+        args.props.accessLogs.enabled = false;
+        await assertHasResourceViolation(policy, args, { message: "ALB LoadBalancers should have access logging configured and enabled." });
+    });
+
+    it("#3", async () => {
+        const args = getResourceValidationArgs();
+        args.props.accessLogs.bucket = undefined;
+        await assertHasResourceViolation(policy, args, { message: "ALB LoadBalancers should have access logging configured and enabled." });
+    });
+});

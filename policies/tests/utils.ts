4720e2ad7736b7089a7d66e50003baa54de1705d
Aurelien Requiem <aurelien@requiem.fr>
2022-12-29 14:18:24 +1100

    ✔ rewrite utils.ts to correctly pass eslint.

--- policies/tests/utils.ts
+++ policies/tests/utils.ts
@@ -12,7 +12,13 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import * as policy from "@pulumi/policy";
+import {
+    ResourceValidationArgs,
+    StackValidationArgs,
+    StackValidationPolicy,
+    PolicyResource,
+    ResourceValidationPolicy,
+} from "@pulumi/policy";
 import * as policies from "../index";
 import { PolicyInfo, PolicyMetadata, policiesManagement, FilterPolicyArgs } from "../utils";
 import { Resource, Unwrap } from "@pulumi/pulumi";
@@ -30,15 +36,17 @@ const empytOptions = {
     additionalSecretOutputs: [],
 };
 
-// createResourceValidationArgs will create a ResourceValidationArgs using the `type` from
-// the specified `resourceClass` and `props` returned from the specified `argsFactory`.
-// The return type of the `argsFactory` is the unwrapped args bag for the resource, inferred
-// from the resource's constructor parameters.
+/**
+ * The function createResourceValidationArgs() will create a ResourceValidationArgs using the `type` from
+ * the specified `resourceClass` and `props` returned from the specified `argsFactory`.
+ * The return type of the `argsFactory` is the unwrapped args bag for the resource, inferred
+ * from the resource's constructor parameters.
+ */
 export function createResourceValidationArgs<TResource extends Resource, TArgs>(
-    resourceClass: { new(name: string, args: TArgs, ...rest: any[]): TResource },
+    resourceClass: { new(name: string, args: TArgs, ...rest: any[]): TResource }, // eslint-disable-line @typescript-eslint/no-shadow
     args: Unwrap<NonNullable<TArgs>>,
     config?: Record<string, any>,
-): policy.ResourceValidationArgs {
+): ResourceValidationArgs {
     const type = (<any>resourceClass).__pulumiType;
     if (typeof type !== "string") {
         assert.fail("Could not determine Pulumi type from resourceClass.");
@@ -61,19 +69,21 @@ export interface PolicyViolation {
     urn?: string;
 }
 
-// createStackValidationArgs will create a StackValidationArgs, simulating a stack that has a
-// single resource with the provided type and properties.
+/**
+ * The function createStackValidationArgs() will create a StackValidationArgs, simulating a stack that has a
+ * single resource with the provided type and properties.
+ */
 export function createStackValidationArgs<TResource extends Resource, TArgs>(
     resourceClass: { new(name: string, args: TArgs, ...rest: any[]): TResource },
     props: any,
     config?: Record<string, any>,
-): policy.StackValidationArgs {
+): StackValidationArgs {
     const type = (<any>resourceClass).__pulumiType;
     if (typeof type !== "string") {
         assert.fail("Could not determine Pulumi type from resourceClass.");
     }
 
-    const testResource: policy.PolicyResource = {
+    const testResource: PolicyResource = {
         type: type as string,
         props: props,
         urn: "unknown",
@@ -88,12 +98,14 @@ export function createStackValidationArgs<TResource extends Resource, TArgs>(
     return {
         resources: [testResource],
         getConfig: <T>() => <T>(config || {}),
-    } as policy.StackValidationArgs;
+    } as StackValidationArgs;
 }
 
-// runResourcePolicy will run some basic checks for a policy's metadata, and then
-// execute its rules with the provided type and properties.
-async function runResourcePolicy(resPolicy: policy.ResourceValidationPolicy, args: policy.ResourceValidationArgs): Promise<PolicyViolation[]> {
+/**
+ * The function runResourcePolicy() will run some basic checks for a policy's metadata, and then
+ * execute its rules with the provided type and properties.
+ */
+async function runResourcePolicy(resPolicy: ResourceValidationPolicy, args: ResourceValidationArgs): Promise<PolicyViolation[]> {
     const violations: PolicyViolation[] = [];
     const report = (message: string, urn?: string) => {
         violations.push({ message: message, urn: urn });
@@ -107,9 +119,11 @@ async function runResourcePolicy(resPolicy: policy.ResourceValidationPolicy, arg
     return violations;
 }
 
-// runStackPolicy will run some basic checks for a policy's metadata, and then
-// execute its rules with the provided type and properties.
-async function runStackPolicy(stackPolicy: policy.StackValidationPolicy, args: policy.StackValidationArgs): Promise<PolicyViolation[]> {
+/**
+ * The function runStackPolicy() will run some basic checks for a policy's metadata, and then
+ * execute its rules with the provided type and properties.
+ */
+async function runStackPolicy(stackPolicy: StackValidationPolicy, args: StackValidationArgs): Promise<PolicyViolation[]> {
     const violations: PolicyViolation[] = [];
     const report = (message: string, urn?: string) => {
         violations.push({ message: message, urn: urn });
@@ -119,8 +133,9 @@ async function runStackPolicy(stackPolicy: policy.StackValidationPolicy, args: p
     return violations;
 }
 
-
-// assertNoViolations runs the policy and confirms no violations were found.
+/**
+ * The function assertNoViolations() runs the policy and confirms no violations were found.
+ */
 function assertNoViolations(allViolations: PolicyViolation[]) {
     if (allViolations && allViolations.length > 0) {
         for (const violation of allViolations) {
@@ -132,7 +147,9 @@ function assertNoViolations(allViolations: PolicyViolation[]) {
 }
 
 
-// assertHasViolation runs the policy and confirms the expected violation is reported.
+/**
+ * The function assertHasViolation() runs the policy and confirms the expected violation is reported.
+ */
 function assertHasViolation(allViolations: PolicyViolation[], wantViolation: PolicyViolation) {
     if (!allViolations || allViolations.length === 0) {
         assert.fail("no violations reported, but expected one");
@@ -166,45 +183,65 @@ function assertHasViolation(allViolations: PolicyViolation[], wantViolation: Pol
     }
 }
 
-// Returns "now", d days in the future or past.
+/**
+ * The function daysFromNow() returns "now", d days in the future or past.
+ */
 export function daysFromNow(days: number): Date {
     const date = new Date();
     date.setDate(date.getDate() + days);
     return date;
 }
 
-export async function assertHasResourceViolation(resPolicy: policy.ResourceValidationPolicy, args: policy.ResourceValidationArgs, wantViolation: PolicyViolation) {
+/**
+ * The function asserts the resource provided reports a violation.
+ */
+export async function assertHasResourceViolation(resPolicy: ResourceValidationPolicy, args: ResourceValidationArgs, wantViolation: PolicyViolation) {
     const allViolations = await runResourcePolicy(resPolicy, args);
     assertHasViolation(allViolations, wantViolation);
 }
 
-export async function assertNoResourceViolations(resPolicy: policy.ResourceValidationPolicy, args: policy.ResourceValidationArgs) {
+/**
+ * The function asserts the resource provided does NOT reports any violation.
+ */
+export async function assertNoResourceViolations(resPolicy: ResourceValidationPolicy, args: ResourceValidationArgs) {
     const allViolations = await runResourcePolicy(resPolicy, args);
     assertNoViolations(allViolations);
 }
 
-export async function assertNoStackViolations(stackPolicy: policy.StackValidationPolicy, args: policy.StackValidationArgs) {
+/**
+ * The function asserts the stack provided reports a violation.
+ */
+export async function assertNoStackViolations(stackPolicy: StackValidationPolicy, args: StackValidationArgs) {
     const allViolations = await runStackPolicy(stackPolicy, args);
     assertNoViolations(allViolations);
 }
 
+/**
+ * The function asserts the stack provided does NOT reports any violation.
+ */
 export async function assertHasStackViolation(
-    stackPolicy: policy.StackValidationPolicy, args: policy.StackValidationArgs, wantViolation: PolicyViolation) {
+    stackPolicy: StackValidationPolicy, args: StackValidationArgs, wantViolation: PolicyViolation) {
     const allViolations = await runStackPolicy(stackPolicy, args);
     assertHasViolation(allViolations, wantViolation);
 }
 
-export function assertResourcePolicyIsRegistered(policy: policy.ResourceValidationPolicy) {
+/**
+ * The function asserts the policy has been registered.
+ */
+export function assertResourcePolicyIsRegistered(policy: ResourceValidationPolicy) {
     if (!policies.policiesManagement.getPolicyByName(policy.name)) {
         assert.fail(`Policy ${policy.name} is not registered.`);
     }
 }
 
-export function assertResourcePolicyName(policy: policy.ResourceValidationPolicy, name: string) {
+/**
+ * The function asserts the policy name is as expected.
+ */
+export function assertResourcePolicyName(policy: ResourceValidationPolicy, name: string) {
     const re = /([a-z]{1}[\da-z\-]+[\da-z]{1})/g;
 
     if (!isLowerCase(policy.name)) {
-        assert.fail(`Policy name '${policy.name}' should be in lower case.`)
+        assert.fail(`Policy name '${policy.name}' should be in lower case.`);
     }
 
     if (policy.name !== name) {
@@ -221,13 +258,19 @@ export function assertResourcePolicyName(policy: policy.ResourceValidationPolicy
     }
 }
 
-export function assertResourcePolicyEnforcementLevel(policy: policy.ResourceValidationPolicy) {
+/**
+ * The function asserts the policy has the correct enforcementLevel.
+ */
+export function assertResourcePolicyEnforcementLevel(policy: ResourceValidationPolicy) {
     if (policy.enforcementLevel !== "advisory") {
         assert.fail(`Policy name '${policy.name}' should have its enforcementLevel set to 'advisory'.`);
     }
 }
 
-export function assertResourcePolicyDescription(policy: policy.ResourceValidationPolicy) {
+/**
+ * The function asserts the policy has a sentence as a description.
+ */
+export function assertResourcePolicyDescription(policy: ResourceValidationPolicy) {
     if (!policy.description) {
         assert.fail(`Policy name '${policy.name}' should have a description.`);
     } else {
@@ -237,7 +280,7 @@ export function assertResourcePolicyDescription(policy: policy.ResourceValidatio
         if (puntuations === null) {
             assert.fail(`The policy '${policy.name}' description requires a complete sentence.`);
         } else {
-            var sentences = policy.description.split(/[.?!](?:\s+|$)/u).map((sentence, idx) => {
+            const sentences = policy.description.split(/[.?!](?:\s+|$)/u).map((sentence, idx) => {
                 // re-add the punctuation back to the sentence
                 if(puntuations[idx]) {
                     return `${sentence}${puntuations[idx]}`;
@@ -258,14 +301,17 @@ export function assertResourcePolicyDescription(policy: policy.ResourceValidatio
     }
 }
 
-export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceValidationPolicy, metadata: PolicyMetadata) {
+/**
+ * The function asserts the policy has the expeced policy metadata.
+ */
+export function assertResourcePolicyRegistrationDetails(policy: ResourceValidationPolicy, metadata: PolicyMetadata) {
     const registeredPolicy: PolicyInfo | undefined = policies.policiesManagement.getPolicyByName(policy.name);
     if (!registeredPolicy) {
         assert.fail(`Policy ${policy.name} is not registered.`);
     }
     if (registeredPolicy) {
         /**
-         * Frameworks
+         * Perform checks on Frameworks.
          */
         if (registeredPolicy.policyMetadata.frameworks && metadata.frameworks
             && registeredPolicy.policyMetadata.frameworks.length && metadata.frameworks.length) {
@@ -276,11 +322,11 @@ export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceV
             if (
                 (!registeredPolicy.policyMetadata.frameworks && metadata.frameworks) ||
                 (registeredPolicy.policyMetadata.frameworks && !metadata.frameworks)) {
-                    assert.fail(`Policy ${policy.name} 'frameworks' don't match.`);
+                assert.fail(`Policy ${policy.name} 'frameworks' don't match.`);
             }
         }
         /**
-         * Services
+         * Perform checks on Services.
          */
         if (registeredPolicy.policyMetadata.vendors && !registeredPolicy.policyMetadata.vendors.includes("kubernetes")) {
             if (registeredPolicy.policyMetadata.services && registeredPolicy.policyMetadata.services.length) {
@@ -302,17 +348,17 @@ export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceV
             }
         }
         /**
-         * Severities
+         * Perform checks on Severities.
          */
         if (registeredPolicy.policyMetadata.severity) {
             switch(registeredPolicy.policyMetadata.severity) {
-                case "low":
-                case "medium":
-                case "high":
-                case "critical":
-                    break;
-                default:
-                    assert.fail(`Policy ${policy.name} 'severity' isn't valid ('low', 'medium', 'high', 'critical').`);
+            case "low":
+            case "medium":
+            case "high":
+            case "critical":
+                break;
+            default:
+                assert.fail(`Policy ${policy.name} 'severity' isn't valid ('low', 'medium', 'high', 'critical').`);
             }
         }
         if (registeredPolicy.policyMetadata.severity && metadata.severity) {
@@ -327,9 +373,9 @@ export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceV
             }
         }
         /**
-         * Topics
+         * Perform checks on Topics.
          */
-         if (registeredPolicy.policyMetadata.topics && metadata.topics
+        if (registeredPolicy.policyMetadata.topics && metadata.topics
             && registeredPolicy.policyMetadata.topics.length && metadata.topics.length) {
             if(!compareArray(registeredPolicy.policyMetadata.topics, metadata.topics)) {
                 assert.fail(`Policy ${policy.name} 'topics' don't match.`);
@@ -342,7 +388,7 @@ export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceV
             }
         }
         /**
-         * Vendors
+         * Perform checks on Vendors.
          */
         if (registeredPolicy.policyMetadata.vendors && registeredPolicy.policyMetadata.vendors.length) {
             if (registeredPolicy.policyMetadata.vendors.length > 1) {
@@ -365,18 +411,27 @@ export function assertResourcePolicyRegistrationDetails(policy: policy.ResourceV
     }
 }
 
+/**
+ * The function asserts some policies have been registered.
+ */
 export function assertHasRegisteredPolicies() {
     if (policiesManagement.getStats().policyCount === 0) {
         assert.fail(`Didn't find any registered policies.`);
     }
 }
 
+/**
+ * The function asserts 1 or more policies are up for selection.
+ */
 export function assertHasRemainingPolicies() {
     if (policiesManagement.getStats().remainingPolicyCount === 0) {
         assert.fail(`Didn't find any remaining policies.`);
     }
 }
 
+/**
+ * The function asserts the number of selectable policies is the same as the total of all registered policies.
+ */
 export function assertHasAllRemainingPolicies() {
     if (policiesManagement.getStats().policyCount < 1) {
         assert.fail(`Registered policies count and remaining policies count don't match. ${policiesManagement.getStats().remainingPolicyCount} policies but ${policiesManagement.getStats().policyCount} are registered.`);
@@ -387,28 +442,40 @@ export function assertHasAllRemainingPolicies() {
     }
 }
 
+/**
+ * The function asserts no more policies are selectable.
+ */
 export function assertHasNoRemainingPolicies() {
     if (policiesManagement.getStats().remainingPolicyCount !== 0) {
         assert.fail(`Found remaining policies but expected none.`);
     }
 }
 
+/**
+ * The function asserts an expected number of policies is selectable.
+ */
 export function assertExpectedRemainingPolicyCount(expectedtedRemainingPolicyCount: number) {
     if (policiesManagement.getStats().remainingPolicyCount !== expectedtedRemainingPolicyCount) {
         assert.fail(`Expected remaining policy counts don't match. Found ${policiesManagement.getStats().remainingPolicyCount} but expeced ${expectedtedRemainingPolicyCount}.`);
     }
 }
 
+/**
+ * The function asserts no policies are selected twice.
+ */
 export function assertNoDoubleSelection(filterPolicy: FilterPolicyArgs) {
     policies.policiesManagement.resetPolicyfilter();
     const firstSelection = policies.policiesManagement.filterPolicies(filterPolicy);
     const secondSelection = policies.policiesManagement.filterPolicies(filterPolicy);
     if (secondSelection.length > 0) {
-        assert.fail(`Some policies haven't been returned after they'd been already selected.`)
+        assert.fail(`Some policies haven't been returned after they'd been already selected.`);
     }
     policies.policiesManagement.resetPolicyfilter();
 }
 
+/**
+ * The function asserts the enforcementLevel is applied when selecting policies.
+ */
 export function assertSelectionEnforcementLevel(filterPolicy: FilterPolicyArgs, enforcementLevel: string) {
     policies.policiesManagement.resetPolicyfilter();
 
@@ -422,12 +489,17 @@ export function assertSelectionEnforcementLevel(filterPolicy: FilterPolicyArgs, 
     policies.policiesManagement.resetPolicyfilter();
 }
 
-// Determine whether the given `input` is a string in lowercase
+/**
+ * Determine whether the given `input` is a string in lowercase.
+ */
 function isLowerCase (input: string) {
-    return input === String(input).toLowerCase()
+    return input === String(input).toLowerCase();
 }
 
-// for context https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript/
+/**
+ * This function compares 2 arrays.
+ * See for context: https://stackoverflow.com/questions/7837456/how-to-compare-arrays-in-javascript/.
+ */
 function compareArray(array1: string[], array2: string[]): boolean {
 
     const array2Sorted = array2.slice().sort();
@@ -436,7 +508,9 @@ function compareArray(array1: string[], array2: string[]): boolean {
     }));
 }
 
-// Helper to check if `type` is the type of `resourceClass`.
+/**
+ * Helper to check if `type` is the type of `resourceClass`.
+ */
 function isTypeOf<TResource extends Resource>(
     type: string,
     resourceClass: { new(...rest: any[]): TResource },

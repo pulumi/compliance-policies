1c79c60c770fd8f794a4578ad2c5b136734ec88c
Aurelien Requiem <aurelien@requiem.fr>
2023-01-17 15:26:28 +1100

    update imports

--- policy-packs/policy-pack-finops/index.ts
+++ policy-packs/policy-pack-finops/index.ts
@@ -1,14 +1,13 @@
-import * as aws from "@pulumi/aws";
 import { PolicyPack } from "@pulumi/policy";
-import * as policies from "@bobcatt/pulumi-policies";
+import { policiesManagement } from "@pulumi-premium-policies/policy-management";
 
 new PolicyPack("policy-pack-acme-corp-finops", {
     policies: [
-        ...policies.policyRegistrations.filterPolicies({
+        ...policiesManagement.filterPolicies({
             vendors:["aws"],
             frameworks: ["pcidss"],
         }),
-        ...policies.policyRegistrations.filterPolicies({
+        ...policiesManagement.filterPolicies({
             vendors:["kubernetes"],
             severities: ["critical"],
         }),

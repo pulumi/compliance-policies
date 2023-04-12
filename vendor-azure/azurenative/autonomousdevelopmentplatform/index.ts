de6b0d64ee8d3b90f1ed1ac0170e19135499f9cb
Aurelien Requiem <aurelien@requiem.fr>
2023-04-12 14:42:48 +1000

    ✨ add another 100 new policies

--- vendor-azure/azurenative/autonomousdevelopmentplatform/index.ts
+++ vendor-azure/azurenative/autonomousdevelopmentplatform/index.ts
@@ -19,3 +19,5 @@
 // SOFTWARE.
 
 export * as v20200701preview from "./v20200701preview";
+export * as v20210201preview from "./v20210201preview";
+export * as v20211101preview from "./v20211101preview";
---
+++ vendor-azure/azurenative/avs/v20200717preview/index.ts
@@ -0,0 +1,32 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Permission is hereby granted to use the Software for the duration
+// of your contract with Pulumi Corporation under the following terms and
+// conditions.
+//
+//       https://www.pulumi.com/terms-and-conditions/
+//
+// By using the Software, you agree not to copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, even after the
+// termination of your contract with us.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+export * as Addon from "./Addon";
+export * as Authorization from "./Authorization";
+export * as Cluster from "./Cluster";
+export * as GlobalReachConnection from "./GlobalReachConnection";
+export * as HcxEnterpriseSite from "./HcxEnterpriseSite";
+export * as PrivateCloud from "./PrivateCloud";
+export * as WorkloadNetworkDhcp from "./WorkloadNetworkDhcp";
+export * as WorkloadNetworkDnsService from "./WorkloadNetworkDnsService";
+export * as WorkloadNetworkDnsZone from "./WorkloadNetworkDnsZone";
+export * as WorkloadNetworkPortMirroring from "./WorkloadNetworkPortMirroring";
+export * as WorkloadNetworkSegment from "./WorkloadNetworkSegment";
+export * as WorkloadNetworkVMGroup from "./WorkloadNetworkVMGroup";

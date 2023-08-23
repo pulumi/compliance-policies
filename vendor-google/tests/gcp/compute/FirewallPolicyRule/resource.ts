e0fa0eb5038e892d6ca17a8f676d36ae8d93ce06
Aurelien Requiem <aurelien@requiem.fr>
2023-08-23 19:17:40 +1000

    ✨ add unit tests for Google policy package

---
+++ vendor-google/tests/gcp/compute/FirewallPolicyRule/resource.ts
@@ -0,0 +1,46 @@
+// Copyright 2016-2023, Pulumi Corporation.
+//
+// Permission is hereby granted to use the Software for the duration
+// of your contract with Pulumi Corporation under the following terms and
+// conditions.
+//
+//       https://www.pulumi.com/terms-and-conditions/
+//
+// By using the Software, you agree not to copy, modify, merge, publish,
+// distribute, sublicense, and/or sell copies of the Software, even after the
+// termination of your contract with us.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import * as gcp from "@pulumi/gcp";
+import { ResourceValidationArgs } from "@pulumi/policy";
+import * as enums from "../../enums";
+import { createResourceValidationArgs } from "@pulumi-premium-policies/unit-test-helpers";
+
+/**
+ * Create a `ResourceValidationArgs` to be process by the unit test.
+ *
+ * @returns A `ResourceValidationArgs`.
+ */
+export function getResourceValidationArgs(): ResourceValidationArgs {
+    return createResourceValidationArgs(gcp.compute.FirewallPolicyRule, {
+        description: "This is a description for gcp.compute.FirewallPolicyRule.",
+        firewallPolicy: "",
+        action: "allow",
+        disabled: false,
+        direction: "INGRESS",
+        match: {
+            layer4Configs: [{
+                ipProtocol: "tcp",
+                ports: ["443"],
+            }],
+        },
+        priority: 1000,
+    });
+}

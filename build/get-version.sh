e427538066aec0bffb8a3937adb636235d885837
Aurelien Requiem <aurelien@requiem.fr>
2024-11-19 16:27:44 +1100

    🌿 drastically simplify get-version.sh
    Previous and current versions are determines based on already existing
    tags, so `-dirty` doesn't randomly show up in tag names.
    For the following version of a given tag, we find the most recent
    version and increment its patch version only while preserving any suffix

--- build/get-version.sh
+++ build/get-version.sh
@@ -5,22 +5,25 @@
 set -o nounset -o errexit -o pipefail
 
 usage() {
-    echo "Usage: get-version.sh [ --tagprefix tag ] [ --commitish git_commitish ] [ --nextversion | --next ]"
+    echo "Usage: get-version.sh [ --tagprefix tag ] [ --commitish git_commitish ] [ --nextversion | --next | --previousversion | --previous]"
     exit 2
 }
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 COMMITISH="HEAD"
 DIRTY_TAG=""
-NEXT_VERSION=0
+# 0 = current version, will be determined based on git tags
+# 1 = next version, the value is computed based on the most recent version.
+# -1 = previous version, will be determined based on git tags, one before last
+REQUESTED_VERSION=0
 
 SHORTOPTS="h"
-LONGOPTS="tagprefix:,commitish:,help,nextversion,next"
+LONGOPTS="tagprefix:,commitish:,help,nextversion,next,previousversion,previous"
 OPTS=$(getopt  --name="$0" --options=$SHORTOPTS --longoptions=$LONGOPTS -- "$@")
 VALID_ARGUMENTS=$# # Returns the count of arguments that are in short or long options
 
 if [ "$VALID_ARGUMENTS" -eq 0 ]; then
-  help
+  usage
 fi
 
 eval set -- "$OPTS"
@@ -39,7 +42,19 @@ while /bin/true; do
             usage
             ;;
         --next | --nextversion)
-            NEXT_VERSION=1
+            if [ "$REQUESTED_VERSION" -ne 0 ]; then
+                echo "options --next(version) and --previous(version) are mutually exclusive"
+                usage
+            fi
+            REQUESTED_VERSION=1
+            shift 1
+            ;;
+        --previous | --previousversion)
+            if [ "$REQUESTED_VERSION" -ne 0 ]; then
+                echo "options --next(version) and --previous(version) are mutually exclusive"
+                usage
+            fi
+            REQUESTED_VERSION=-1
             shift 1
             ;;
         --)
@@ -57,47 +72,39 @@ if [ -z "$TAG_PREFIX" ]; then
     usage
 fi
 
-# Figure out if the worktree is dirty, we run update-index first
-# as we've seen cases in Travis where not doing so causes git to
-# treat the worktree as dirty when it is not.
-git update-index -q --refresh
-if ! git diff-files --quiet .; then
-    DIRTY_TAG="dirty"
+# When requesting the next minor version,
+if [ "$REQUESTED_VERSION" -eq 1 ]; then
+    # Figure out if the worktree is dirty, we run update-index first
+    # as we've seen cases in Travis where not doing so causes git to
+    # treat the worktree as dirty when it is not.
+    git update-index -q --refresh
+    if ! git diff-files --quiet .; then
+        DIRTY_TAG="dirty"
+    fi
 fi
 
-# If we have an exact tag, just use it.
-# ⚠️ This commented block may notwork if the last commit has a tag
-#    on it that belongs to another policy package.
-# if git describe --tags --exact-match "${COMMITISH}" >/dev/null 2>&1; then
-#     echo -n "$(git describe --tags --exact-match "${COMMITISH}")"
-#     if [ ! -z "${DIRTY_TAG}" ]; then
-#         echo -n "+${DIRTY_TAG}"
-#     fi
-#     echo ""
-#     exit 0
-# fi
+case "$REQUESTED_VERSION" in
+    "1")
+        TAG="$(git for-each-ref --sort=creatordate --format '%(refname:short)' "refs/tags/${TAG_PREFIX}-*" | sed "s/${TAG_PREFIX}-//;" | tail -n 1)"
+        if [ -z "$TAG" ]; then
+            TAG="0.0.0"
+        fi
+        MAJOR="$(cut -d. -f1 <<< "${TAG}")"
+        MINOR="$(cut -d. -f2 <<< "${TAG}")"
+        PATCH="$(cut -d. -f3 <<< "${TAG}")"
 
-# Otherwise, increment the patch version, add the -dev tag and some
-# commit metadata. If there's no existing tag, pretend a v0.0.0 was
-# there so we'll produce v0.0.1-dev builds.
-if git describe --tags --abbrev=0 --match "${TAG_PREFIX}-*" "${COMMITISH}" > /dev/null 2>&1; then
-    TAG=$(git describe --tags --abbrev=0 --match "${TAG_PREFIX}-*" "${COMMITISH}")
-else
-    TAG="${TAG_PREFIX}-0.0.0"
-fi
+        # We want to include some additional information. To the base tag we
+        # add a timestamp and commit hash. We use the timestamp of the commit
+        # itself, not the date it was authored (so it will change when someone
+        # rebases a PR into master, for example).
+        echo "${MAJOR}.${MINOR}.$((${PATCH}+${REQUESTED_VERSION}))${DIRTY_TAG:++$DIRTY_TAG}"
+        ;;
+    "0")
+        git for-each-ref --sort=creatordate --format '%(refname:short)' "refs/tags/${TAG_PREFIX}-*" | sed "s/${TAG_PREFIX}-//;" | tail -n 1
+        ;;
+    "-1")
+        git for-each-ref --sort=creatordate --format '%(refname:short)' "refs/tags/${TAG_PREFIX}-*" | sed "s/${TAG_PREFIX}-//;" | tail -n 2 | head -n 1
+        ;;
+esac
 
-# We remove prefix from the tag to expose the bare-ish version
-TAG="${TAG##*-}"
-# We remove the suffix off any pre-release tag (e.g. from doing a -rc build)
-TAG=${TAG%%-*}
 
-MAJOR=$(cut -d. -f1 <<< "${TAG}")
-MINOR=$(cut -d. -f2 <<< "${TAG}")
-PATCH=$(cut -d. -f3 <<< "${TAG}")
-
-# We want to include some additional information. To the base tag we
-# add a timestamp and commit hash. We use the timestamp of the commit
-# itself, not the date it was authored (so it will change when someone
-# rebases a PR into master, for example).
-# echo -n "${MAJOR}.${MINOR}.$((${PATCH}+1))-dev.$(git show -s --format='%ct+g%h' ${COMMITISH})"
-echo "${MAJOR}.${MINOR}.$((${PATCH}+${NEXT_VERSION}))${DIRTY_TAG:++$DIRTY_TAG}"

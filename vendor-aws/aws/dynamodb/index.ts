d9cb0234b922055a48f92de11feaf43f3c6882e8
matfor-wizards <matheus.fornazieri@c.wizards.com>
2025-08-25 17:31:02 -0300

    fix: wrong export path

--- vendor-aws/aws/dynamodb/index.ts
+++ vendor-aws/aws/dynamodb/index.ts
@@ -12,4 +12,4 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-export * as Instance from "./Instance";
+export * as Instance from "./Table";
